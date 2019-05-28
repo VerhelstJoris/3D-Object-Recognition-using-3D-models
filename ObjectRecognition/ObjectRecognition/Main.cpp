4120d8d7a700786c399a0c75f2cd6413b63ed697
VerhelstJoris <megajorver@gmail.com>
2019-05-28 15:42:52 +0100

    drawing a triangle

--- ObjectRecognition/ObjectRecognition/Main.cpp
+++ ObjectRecognition/ObjectRecognition/Main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <glm.hpp>
 
+
 #include "OpenCV/DisplayWindow.h"
 #include "OpenCV/ImageOperations.h"
 #include "OpenCV/HelperFunctions.h"
@@ -10,6 +11,7 @@ using namespace cv;
 using namespace std;
 
 void ProcessUserInput(GLFWwindow *window);
+void Draw(GLuint vBuffer);
 
 int main(void)
 {
@@ -65,6 +67,8 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
+	glfwWindowHint(GLFW_SAMPLES, 4);					//amount of samples per pixel
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);		//set to version 3.3 of opengl
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	//only use core profile
@@ -79,11 +83,36 @@ int main(void)
 	}
 	glfwMakeContextCurrent(window);
 
-	glViewport(0, 0, 800, 600);		//specify the size of the rendering window
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
 
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = HelperFunctions::LoadShaders("OpenGL/Shaders/VertexShader.glsl", "OpenGL/Shaders/FragmentShader.glsl");
+
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
 
 
 	/* Loop until the user closes the window */
@@ -94,12 +123,20 @@ int main(void)
 
 		//RENDERING
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);		//set color
-		glClear(GL_COLOR_BUFFER_BIT);				//clear using previously set color
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(programID);
+
+		Draw(vertexbuffer);
 
 		//SWAP BUFFERS
 		glfwSwapBuffers(window);
 
 	}
+
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
 
 	glfwTerminate();
 	return 0;
@@ -115,4 +152,22 @@ void ProcessUserInput(GLFWwindow *window)
 
 	/* Poll for and process events */
 	glfwPollEvents();
+}
+
+void Draw(GLuint vBuffer)
+{
+	// 1st attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+	// Draw the triangle !
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+	glDisableVertexAttribArray(0);
 }
